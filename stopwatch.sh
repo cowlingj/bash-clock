c627fd40dc30893a8bd7b6c90ed5aa15c5108507
Jonathan Cowling <09cowlingj@gmail.com>
2018-06-10 19:33:33 +0100

    added gui support

--- stopwatch.sh
+++ stopwatch.sh
@@ -14,7 +14,8 @@ PROGRAM="$0"
 DEFAULT_INTERVAL="1s"
 
 function usage {
-  echo "$PROGRAM [-i|--interval interval] [-p|--precision] [-h --help]"
+  echo "$PROGRAM [-i|--interval interval] [-p|--precision] " \
+    "[-h --help] [-g|--gui]"
   return 0
 }
 
@@ -25,6 +26,7 @@ function graceful_stop {
 
 INTERVAL=""
 PRECISION=""
+GUI=1
 
 # parse args
 while [ "$#" -gt "0" ]; do
@@ -56,6 +58,14 @@ while [ "$#" -gt "0" ]; do
       INTERVAL="$2"
       shift 2
       ;;
+    "-g") ;&
+    "--gui")
+      if [ "${#TIME[@]}" -ne "0" ] || [ "$GUI" -eq "0" ]; then usage; exit 22; fi
+      zenity --version > "/dev/null"
+      GUI=$?
+      [ "$GUI" -ne "0" ] && echo "warn: zenity interface not supported, continuing without"
+      shift
+      ;;
     *)
       usage;
       exit 22;
@@ -71,14 +81,24 @@ trap graceful_stop SIGINT
 TIME="0"
 INTERVAL_SECS="$(time_to_sec "$INTERVAL")"
 [ -z "$PRECISION" ] && PRECISION="$(bc <<< "scale($INTERVAL_SECS)")"
-echo "p=$PRECISION" >&2
 
 # set time to the time elapsed since the start of execution every INTERVAL
 # print time as a string on the same line each interval
-while true; do
+[ "$GUI" -eq "0" ] &&
+(
+  echo "100"
+  while true; do
+    CURR_TS="$(date "+%s.%N")"
+    TIME="$(bc <<< "scale=$PRECISION; ($CURR_TS - $START_TIME)/1")"
+    echo "#$(sec_to_time "$TIME")"
+    sleep "$INTERVAL"
+  done
+) | zenity --progress --title="$0" --pulsate --no-cancel --ok-label="stop" 2>"/dev/null"
+[ "$GUI" -eq "0" ] && graceful_stop
+
+[ "$GUI" -ne "0" ] && while true; do
   CURR_TS="$(date "+%s.%N")"
   TIME="$(bc <<< "scale=$PRECISION; ($CURR_TS - $START_TIME)/1")"
   echo -ne "\r$(sec_to_time "$TIME")\e[0K"
   sleep "$INTERVAL"
 done
-
--- timer.sh
+++ timer.sh
@@ -15,7 +15,7 @@ DEFAULT_INTERVAL="1s"
 
 function usage {
   echo "$PROGRAM [-i|--interval interval] "\
-    "[-p|--precision precision] [-h|--help] time..."
+    "[-p|--precision precision] [-h|--help] [-g|--gui] time..."
   return 0
 }
 
@@ -27,6 +27,7 @@ function graceful_exit {
 INTERVAL=""
 TIME=()
 PRECISION=""
+GUI=1
 
 # parse args
 while [ "$#" -gt "0" ]; do
@@ -58,6 +59,14 @@ while [ "$#" -gt "0" ]; do
       INTERVAL="$2"
       shift 2
       ;;
+    "-g") ;&
+    "--gui")
+      if [ "${#TIME[@]}" -ne "0" ] || [ "$GUI" -eq "0" ]; then usage; exit 22; fi
+      zenity --version > "/dev/null"
+      GUI=$?
+      [ "$GUI" -ne "0" ] && echo "warn: zenity interface not supported, continuing without"
+      shift
+      ;;
     *)
       if [ ! "$(is_num "$1" "ut")" ]; then usage; exit 22; fi
       TIME+=("$1")
@@ -77,16 +86,28 @@ INTERVAL_SECS="$(time_to_sec "$INTERVAL")"
 END_TS="$(bc <<< "$START_TIME + $TIME_LEFT")"
 [ -z "$PRECISION" ] && PRECISION="$(bc <<< "scale($INTERVAL_SECS)")"
 
+# GUI and NON GUI version of:
 # each interval:
 # set time left to end time - time elapsed since the start of execution
 # print time as a formatted string on the same line
 # stop when seconds <= 0
-while true; do
+[ "$GUI" -eq "0" ] && while true; do
   CURR_TS="$(date "+%s.%N")"
   TIME_LEFT="$(bc <<< "scale=$PRECISION; ($END_TS - $CURR_TS)/1")"
   [ "$(bc <<< "$TIME_LEFT <= 0")" -eq "1" ] && TIME_LEFT="0"
-  echo -ne "\r$(sec_to_time "$TIME_LEFT")\e[0K"
+  echo "#$(sec_to_time "$TIME_LEFT")"
+  echo "$(bc <<< "scale=0; 100 * ($CURR_TS - $START_TIME)/($END_TS - $START_TIME)")"
   [ "$TIME_LEFT" == "0" ] && graceful_exit
   sleep "$INTERVAL"
-done
+done | zenity --progress --title="$0" --cancel-label="stop" --ok-label="running..." --percentage=0 --auto-close 2>"/dev/null"
+[ "$GUI" -eq "0" ] && exit "0"
 
+[ "$GUI" -ne "0" ] &&
+  while true; do
+    CURR_TS="$(date "+%s.%N")"
+    TIME_LEFT="$(bc <<< "scale=$PRECISION; ($END_TS - $CURR_TS)/1")"
+    [ "$(bc <<< "$TIME_LEFT <= 0")" -eq "1" ] && TIME_LEFT="0"
+    echo -ne "\r$(sec_to_time "$TIME_LEFT")\e[0K"
+    [ "$TIME_LEFT" == "0" ] && graceful_exit
+    sleep "$INTERVAL"
+  done
